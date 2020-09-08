dfb8f6469d1ce73dbf7b824249d3c1d89bf2a0dc
Hanh <lehonghanh.120@gmail.com>
2020-09-09 00:47:41 +0700

    form ok

--- todos-app-with-backend/src/components/form.js
+++ todos-app-with-backend/src/components/form.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
 
 class Form extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            value: ''
+            value: '',
+            error: null
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -18,7 +20,35 @@ class Form extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
+
+        const input = this.refs.input;
+        const task = input.value;
+        console.log(task);
+        const validateInput = this.validateInput(task);
+
+        if(validateInput) {
+            this.setState({error: validateInput});
+            return false;
+        }
+
+        this.setState({error: null});
+        this.refs.input.value = '';
     };
+
+    componentDidMount() {
+        this.refs.input.focus();
+    }
+
+    renderError() {
+        if(!this.state.error) {
+            return null;
+        }
+        return (
+            <p style={{padding: '5px 10px',
+                       background: '#d9534f',
+                       color:'#fff'}}>{this.state.error}</p>
+        )
+    }
 
     render() {
         return (
@@ -27,16 +57,30 @@ class Form extends Component {
                     <div className="col-md-10">
                         <input className="form-control"
                                type="text"
-                               value={this.state.value}
+                               // value={this.state.value}
                                onChange={this.handleChange}
-                               placeholder="What needs to be done?"/>
+                               placeholder="What needs to be done?"
+                               ref="input"/>
                     </div>
                     <div className="col-md-2">
                         <button type="submit" className="btn btn-default">Create</button>
                     </div>
                 </div>
+                {this.renderError()}
             </form>
         )
+    }
+
+    validateInput = (task) => {
+        if(!task){
+            return "Please enter a task";
+        }
+        else if (_.find(this.props.todos, todo => todo.task === task)){
+            return "Task already exist";
+        }
+        else {
+            return null;
+        }
     }
 };
 
